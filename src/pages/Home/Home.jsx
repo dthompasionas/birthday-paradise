b58476cd9f24bf14ca3bc0a82503e12b64c317c7
Damian Thompasionas <dthompasionas@gmail.com>
2023-08-25 12:05:45 -0400

    added folder structure

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+// import MainLayout from "../../layout/MainLayout";
+import Timer from "../../components/Timer";
+
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <Timer />
+    </>
+  );
+}
+
+export default Home;
